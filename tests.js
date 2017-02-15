5a7bc888553b566f8d5ecbbdb1da4f679a139618
Adina Epstein <Adina-Epstein@Adinas-MacBook-Pro.local>
2017-02-15 13:45:17 -0800

    initial files added

---
+++ tests.js
@@ -0,0 +1,20 @@
+QUnit.test("Test the getAreaCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getAreaCode(num);
+    assert.deepEqual(result, "415", "Valid area code test passed.");
+});
+
+QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
+    assert.throws( function() {
+        getAreCode("415)444-5555");
+    }, "Missing '('. An error should have been thrown." );
+
+    assert.throws( function() {
+        getAreCode("(415 444-5555");
+    }, "Missing ')'. An error should have been thrown." );
+});
+
+QUnit.test( "A title for the test", function( assert ) {
+    assert.throws( function() {
+        yourFunction(badInput);
+    }, "Message identifying the test succeeded in catching the error." );
